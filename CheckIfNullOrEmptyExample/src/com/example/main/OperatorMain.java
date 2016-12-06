ec06d705273c5dd774776f3983ac958833c78cf9
jodl <stefan.jodl@edu.fh-joanneum.at>
2016-12-06 20:23:48 +0100

    added Readme.md improved codestyle

--- CheckIfNullOrEmptyExample/src/com/example/main/OperatorMain.java
+++ CheckIfNullOrEmptyExample/src/com/example/main/OperatorMain.java
@@ -2,19 +2,26 @@ package com.example.main;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Nullsafe check of List
+ * @author Ignis
+ *
+ */
 public class OperatorMain
 {
 
     public static void main(String[] args)
     {
-	List<Integer> tmp1 = new ArrayList<>();
-	checkIfNullOrEmpty(tmp1);
-
 	
+	System.out.println("-null ArrayList");
 	List<Integer> tmp2 = null;
 	checkIfNullOrEmpty(tmp2);
-
+	
+	System.out.println("-Empty ArrayList");
+	List<Integer> tmp1 = new ArrayList<>();
+	checkIfNullOrEmpty(tmp1);
+	
+	System.out.println("-Full ArrayList");
 	List<Integer> tmp3 = new ArrayList<>();
 	tmp3.add(1);
 	checkIfNullOrEmpty(tmp3);
@@ -22,14 +29,15 @@ public class OperatorMain
     
     }
     
-    
-    
-    
+    /**
+     * Method for Checkin an list if its empty or null
+     * @param list
+     */
     private static void checkIfNullOrEmpty(List list){
-	if (list==null || list.size() ==0 ){
-	    System.out.println("Empty oder null");
+	if (list==null || list.size()==0 ){
+	    System.out.println("List is empty oder null\n");
 	} else {
-	    System.out.println("You now can safe handle varible");
+	    System.out.println("You now can safe handle varible\n");
 	}
     }
     
